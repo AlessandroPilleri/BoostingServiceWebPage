4c1452febb5cdd262a04ca960c297953ad13b90b
Alessandro Pilleri <apilleri@outlook.com>
2022-01-15 12:10:00 +0100

    unstable update (price change)

--- public/js/script.js
+++ public/js/script.js
@@ -18,8 +18,7 @@ $('.navbar a').click(function(event) {
  *  - 0: rank boosting
  *  - 1: win boosting
  *  - 2: tournament boosting
- *  - 3: placements
- *  - 4: seasonal reward wins
+ *  - 3: seasonal reward wins
  * 
  * RANKS:
  * maps[0] < rank, price >
@@ -50,7 +49,6 @@ maps[0] = new Map();
 maps[1] = new Map();
 maps[2] = new Map();
 maps[3] = new Map();
-maps[4] = new Map();
 var queue = 0;
 var platform = 0;
 var playwithbooster = false;
@@ -61,91 +59,67 @@ maps[0].set('0', 0); // Bronze 1
 maps[0].set('1', 1); // Bronze 2
 maps[0].set('2', 3); // Bronze 3
 maps[0].set('3', 5); // Silver 1
-maps[0].set('4', 8); // Silver 2
-maps[0].set('5', 10); // Silver 3
-maps[0].set('6', 15); // Gold 1
-maps[0].set('7', 20); // Gold 2
-maps[0].set('8', 25); // Gold 3
-maps[0].set('9', 30); // Plat 1
-maps[0].set('10', 40); // Plat 2
-maps[0].set('11', 50); // Plat 3
-maps[0].set('12', 65); // Diamond 1
-maps[0].set('13', 80); // Diamond 2
-maps[0].set('14', 100); // Diamond 3
-maps[0].set('15', 125); // Champ 1
-maps[0].set('16', 150); // Champ 2
-maps[0].set('17', 180); // Champ 3
-maps[0].set('18', 200); // GC 1
-maps[0].set('19', 250); // GC 2
-maps[0].set('20', 310); // GC 3
-maps[0].set('21', 400); // SSL
+maps[0].set('4', 7); // Silver 2
+maps[0].set('5', 9); // Silver 3
+maps[0].set('6', 11); // Gold 1
+maps[0].set('7', 13); // Gold 2
+maps[0].set('8', 15); // Gold 3
+maps[0].set('9', 17); // Plat 1
+maps[0].set('10', 20); // Plat 2
+maps[0].set('11', 23); // Plat 3
+maps[0].set('12', 25); // Diamond 1
+maps[0].set('13', 28); // Diamond 2
+maps[0].set('14', 30); // Diamond 3
+maps[0].set('15', 35); // Champ 1
+maps[0].set('16', 40); // Champ 2
+maps[0].set('17', 45); // Champ 3
+maps[0].set('18', 50); // GC 1
+maps[0].set('19', 55); // GC 2
+maps[0].set('20', 70); // GC 3
+maps[0].set('21', 100); // SSL
 
 maps[1].set('0', 1); // Bronze 1
 maps[1].set('1', 1); // Bronze 2
 maps[1].set('2', 1); // Bronze 3
-maps[1].set('3', 2); // Silver 1
-maps[1].set('4', 2); // Silver 2
-maps[1].set('5', 2); // Silver 3
-maps[1].set('6', 3); // Gold 1
-maps[1].set('7', 3); // Gold 2
-maps[1].set('8', 3); // Gold 3
-maps[1].set('9', 4); // Plat 1
-maps[1].set('10', 4); // Plat 2
-maps[1].set('11', 4); // Plat 3
-maps[1].set('12', 4); // Diamond 1
-maps[1].set('13', 4); // Diamond 2
-maps[1].set('14', 4); // Diamond 3
-maps[1].set('15', 5); // Champ 1
-maps[1].set('16', 5); // Champ 2
-maps[1].set('17', 5); // Champ 3
-maps[1].set('18', 6); // GC 1
-maps[1].set('19', 6); // GC 2
-maps[1].set('20', 8); // GC 3
-maps[1].set('21', 10); // SSL
+maps[1].set('3', 1); // Silver 1
+maps[1].set('4', 1); // Silver 2
+maps[1].set('5', 1); // Silver 3
+maps[1].set('6', 1); // Gold 1
+maps[1].set('7', 1); // Gold 2
+maps[1].set('8', 1); // Gold 3
+maps[1].set('9', 1); // Plat 1
+maps[1].set('10', 1); // Plat 2
+maps[1].set('11', 1); // Plat 3
+maps[1].set('12', 1); // Diamond 1
+maps[1].set('13', 1); // Diamond 2
+maps[1].set('14', 1); // Diamond 3
+maps[1].set('15', 1); // Champ 1
+maps[1].set('16', 2); // Champ 2
+maps[1].set('17', 2); // Champ 3
+maps[1].set('18', 2); // GC 1
+maps[1].set('19', 2); // GC 2
+maps[1].set('20', 2); // GC 3
+maps[1].set('21', 3); // SSL
 
-maps[2].set('0', 10); // Bronze 
-maps[2].set('1', 14); // Silver 
-maps[2].set('2', 20); // Gold 
-maps[2].set('3', 25); // Plat 
-maps[2].set('4', 30); // Diamond 
-maps[2].set('5', 45); // Champ 
-maps[2].set('6', 70); // GC 
-maps[2].set('7', 100); // SSL
+maps[2].set('0', 5); // Bronze 
+maps[2].set('1', 5); // Silver 
+maps[2].set('2', 5); // Gold 
+maps[2].set('3', 7); // Plat 
+maps[2].set('4', 10); // Diamond 
+maps[2].set('5', 15); // Champ 
+maps[2].set('6', 20); // GC 
+maps[2].set('7', 50); // SSL
 
-maps[3].set('0', 1); // Bronze 
-maps[3].set('1', 2); // Silver 
-maps[3].set('2', 2); // Gold 
-maps[3].set('3', 3); // Plat 
-maps[3].set('4', 3); // Diamond 
-maps[3].set('5', 4); // Champ 1
-maps[3].set('6', 4); // Champ 2
-maps[3].set('7', 4); // Champ 3
-maps[3].set('8', 6); // GC 1
+maps[3].set('0', 10); // Bronze
+maps[3].set('1', 10); // Silver
+maps[3].set('2', 10); // Gold
+maps[3].set('3', 10); // Plat
+maps[3].set('4', 10); // Diamond
+maps[3].set('5', 10); // Champ
+maps[3].set('6', 20); // GC
+maps[3].set('7', 30); // SSL
 
-maps[4].set('0', 1); // Bronze 1
-maps[4].set('1', 1); // Bronze 2
-maps[4].set('2', 1); // Bronze 3
-maps[4].set('3', 2); // Silver 1
-maps[4].set('4', 2); // Silver 2
-maps[4].set('5', 2); // Silver 3
-maps[4].set('6', 3); // Gold 1
-maps[4].set('7', 3); // Gold 2
-maps[4].set('8', 3); // Gold 3
-maps[4].set('9', 4); // Plat 1
-maps[4].set('10', 4); // Plat 2
-maps[4].set('11', 4); // Plat 3
-maps[4].set('12', 4); // Diamond 1
-maps[4].set('13', 4); // Diamond 2
-maps[4].set('14', 4); // Diamond 3
-maps[4].set('15', 5); // Champ 1
-maps[4].set('16', 5); // Champ 2
-maps[4].set('17', 5); // Champ 3
-maps[4].set('18', 6); // GC 1
-maps[4].set('19', 6); // GC 2
-maps[4].set('20', 8); // GC 3
-maps[4].set('21', 10); // SSL
-
-var selectMap = ['#rank', '#win', '#t', '#p', '#sr'];
+var selectMap = ['#rank', '#win', '#t', '#sr'];
 
 function checkQueue(target) {
   if (target == 'rank') {
@@ -166,14 +140,6 @@ function checkQueue(target) {
   }
   if (target == 't') {
     if ( $('#t2').find('option:selected').val() == 0 ) {
-      $("#pwb").prop('checked', false);
-      $("#pwb").attr('disabled', true);
-    } else {
-      $("#pwb").removeAttr('disabled');
-    }
-  }
-  if (target == 'p') {
-    if ( $('#p3').find('option:selected').val() == 0 ) {
       $("#pwb").prop('checked', false);
       $("#pwb").attr('disabled', true);
     } else {
@@ -225,14 +191,9 @@ $('#tcard').click(() => {
   modality = 2;
 })
 
-$('#pcard').click(() => {
-  replace('p');
-  modality = 3;
-})
-
 $('#srcard').click(() => {
   replace('sr');
-  modality = 4;
+  modality = 3;
 }) 
 
 // Create your boost: platform
@@ -296,7 +257,7 @@ function calculatePrice() {
     price = (desiredrank - currentrank) + adds;
   }
 
-  if (modality == 1 || modality == 3 || modality == 4) { // Win boosting / Placements / Seasonal Reward
+  if (modality == 1) { // Win boosting / Placements / Seasonal Reward
     // Current rank price
     var currentrank = maps[modality].get( $(selectMap[modality] + "1").find("option:selected").val().toString() );
 
@@ -312,7 +273,7 @@ function calculatePrice() {
     price = currentrank + adds;
   }
   
-  if (modality == 2) { // Tournament boosting
+  if (modality == 2 || modality == 3) { // Tournament boosting
     // Current rank price
     var currentrank = maps[modality].get( $(selectMap[modality] + "1").find("option:selected").val().toString() );
 
@@ -330,10 +291,11 @@ function calculatePrice() {
 
   // Price
   if (playwithbooster == true && queue != 0) {
-    console.log("pwb = " + (price * 40 / 100))
-    price += price * 40 / 100;
+    console.log("pwb = " + (price * 10 / 100))
+    price += price * 10 / 100;
   }
   
+  (Math.round(price * 100) / 100).toFixed(2); // 2 dec format
   console.log(price);
   $("#price").text("€ " + price);
 }
@@ -410,7 +372,7 @@ function checkout() {
       console.log(res);
     })
   }
-  if (modality == 1 || modality == 3 || modality == 4) {
+  if (modality == 1) {
     axios.post('/checkout', {
       "username": username,
       "modality": modality,
@@ -424,7 +386,7 @@ function checkout() {
       console.log(res);
     })
   }
-  if (modality == 2) {
+  if (modality == 2 || modality == 3) {
     axios.post('/checkout', {
       "username": username,
       "modality": modality,
